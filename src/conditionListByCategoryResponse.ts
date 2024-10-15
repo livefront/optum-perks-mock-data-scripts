d988adafcfb6600fd232983654b3af0067a4880b
meganpekarek <megan@livefront.com>
2024-10-15 15:51:15 -0500

    more realistic mock data

--- src/conditionListByCategoryResponse.ts
+++ src/conditionListByCategoryResponse.ts
@@ -1,21 +1,34 @@
 import { OCApi } from "./api-types";
-import { faker } from "@faker-js/faker";
+import conditions from "./mock-data/conditions";
+import conditionCategories from "./mock-data/conditionCategories";
 
-const getConditions = (amount: number): OCApi.Schemas.ConditionSummary[] => {
-  const conditions = new Array(amount).fill(null);
-  return conditions.map((condition, index) => ({
-    label: index.toString(),
-    name: faker.food.dish(),
-    primaryDescription: faker.food.description(),
-    primaryImagePath: "",
-    urlSlug: "",
+const getConditions = (
+  amount: number,
+  indexToStartFrom: number
+): OCApi.Schemas.ConditionSummary[] => {
+  const emptyConditions = new Array(amount).fill(null);
+  return emptyConditions.map((_, index) => {
+    const condition = conditions[indexToStartFrom + index];
+    return {
+      label: condition.urlSlug,
+      name: condition.name,
+      primaryDescription: condition.description,
+      primaryImagePath: "",
+      urlSlug: condition.urlSlug,
+    };
+  });
+};
+
+const getConditionListByCategory = (
+  amount: number
+): OCApi.Schemas.ConditionListByCategory => {
+  const emptyConditionList = new Array(amount).fill(null);
+  return emptyConditionList.map((_, index) => ({
+    name: conditionCategories[index],
+    fabricId: index,
+    conditions: getConditions(4, index * 4),
   }));
 };
 
-export const response: OCApi.Schemas.ConditionListByCategory = [
-  {
-    name: "Cars",
-    fabricId: 1234,
-    conditions: getConditions(8),
-  },
-];
+export const response: OCApi.Schemas.ConditionListByCategory =
+  getConditionListByCategory(6);
