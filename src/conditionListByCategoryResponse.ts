ec737e76ff2379abff259af2a704c90cbf9652d2
meganpekarek <megan@livefront.com>
2024-10-15 13:16:01 -0500

    able to write typescript objects into json config

---
+++ src/conditionListByCategoryResponse.ts
@@ -0,0 +1,31 @@
+import { OCApi } from "./api-types";
+
+export const response: OCApi.Schemas.ConditionListByCategory = [
+  {
+    name: "Cars",
+    fabricId: 1234,
+    conditions: [
+      {
+        label: "1",
+        name: "Mustang",
+        primaryDescription: "",
+        primaryImagePath: "",
+        urlSlug: "",
+      },
+      {
+        label: "1",
+        name: "Focus",
+        primaryDescription: "",
+        primaryImagePath: "",
+        urlSlug: "",
+      },
+      {
+        label: "1",
+        name: "Taurus",
+        primaryDescription: "",
+        primaryImagePath: "",
+        urlSlug: "",
+      },
+    ],
+  },
+];
