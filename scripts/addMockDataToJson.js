473ca424a43c29c8b5226e46ae95a2d5cbba274e
meganpekarek <megan@livefront.com>
2024-10-15 13:16:01 -0500

    able to write typescript objects into json config

---
+++ scripts/addMockDataToJson.js
@@ -0,0 +1,23 @@
+const fs = require("fs");
+const path = require("path");
+
+const { response } = require("../src/conditionListByCategoryResponse");
+const jsonData = require("../src/mockoonFile.json");
+
+// Specify the path to the output JSON file
+const outputPath = path.join(__dirname, "../src/mockoonFile.json");
+
+jsonData.routes.forEach((route) => {
+  if (route.endpoint === "conditions") {
+    route.responses[0] = response;
+  }
+});
+
+// Write the object to a JSON file
+fs.writeFile(outputPath, JSON.stringify(jsonData, null, 2), (err) => {
+  if (err) {
+    console.error("Error writing JSON file:", err);
+  } else {
+    console.log("JSON file has been saved:", outputPath);
+  }
+});
