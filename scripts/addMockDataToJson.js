76121a595da18fd865d7a7acf5da8c6e0cf0b63c
meganpekarek <megan@livefront.com>
2024-10-17 13:21:15 -0500

    move condition detail file generation

--- scripts/addMockDataToJson.js
+++ scripts/addMockDataToJson.js
@@ -1,34 +1,10 @@
 const fs = require("fs");
 const path = require("path");
-
-const {
-  generateDetailsForAllConditions,
-} = require("../src/responses/conditions/condition-legacy");
 const jsonData = require("../src/mockoonFile.json");
 const { updateAllRoutes } = require("../src/route-transforms/updateAllRoutes");
 
-const detailsForAllConditions = generateDetailsForAllConditions();
-const conditionDetailsOutputPath = (conditionName) =>
-  path.join(
-    __dirname,
-    `../src/generated-json/detailsForConditions/${conditionName}.json`
-  );
-detailsForAllConditions.forEach((conditionDetail) => {
-  const conditionOutputPath = conditionDetailsOutputPath(
-    conditionDetail.conditionDetails.urlSlug
-  );
-  fs.writeFile(
-    conditionOutputPath,
-    JSON.stringify(conditionDetail, null, 2),
-    (err) => {
-      if (err) {
-        console.error("Error writing JSON file:", err);
-      } else {
-        console.log("JSON file has been saved:", conditionOutputPath);
-      }
-    }
-  );
-});
+// Generate JSON files for spec compliant condition details
+require("child_process").fork("./scripts/generateMockConditionFiles.js");
 
 // Update the data in the JSON file to use the mock data and syntax we need
 updateAllRoutes();
